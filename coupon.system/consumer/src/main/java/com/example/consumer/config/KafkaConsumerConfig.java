53de9145b0640397f6e19fdba24fc1b852b9736e
skykchmin <skykchmin@naver.com>
2024-12-12 12:09:38 +0900

    [241212] fix: Kafka listener config 수정

--- coupon.system/consumer/src/main/java/com/example/consumer/config/KafkaConsumerConfig.java
+++ coupon.system/consumer/src/main/java/com/example/consumer/config/KafkaConsumerConfig.java
@@ -20,7 +20,7 @@ public class KafkaConsumerConfig {
 
         Map<String, Object> config = new HashMap<>();
 
-        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9529");
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         config.put(ConsumerConfig.GROUP_ID_CONFIG, "group_1");
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, LongDeserializer.class);
